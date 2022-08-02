eb54a6dc93291a33d99826ffdf64fb4e5deb7089
Henry Smith <henry.smith@netlify.com>
2022-08-02 12:38:21 +0100

    Test

--- netlify/edge-functions/sitemap.js
+++ netlify/edge-functions/sitemap.js
@@ -4,7 +4,7 @@ export default (request, context) => {
   });
 
   const slug = pattern.exec(request.url)?.pathname?.groups?.category;
-  console.log(`/.netlify/builders/sitemap/${slug}`);
+
   return pattern.test(request.url)
     ? context.rewrite(`/.netlify/builders/sitemap/${slug}`)
     : undefined;
